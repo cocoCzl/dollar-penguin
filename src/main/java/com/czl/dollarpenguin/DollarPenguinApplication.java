77bf3830052ec9caa39b880b23a2203e5a12f084
cococzl <cai.ziliang@jy.cocm>
2024-02-20 16:59:34 +0800

    start

---
+++ src/main/java/com/czl/dollarpenguin/DollarPenguinApplication.java
@@ -0,0 +1,13 @@
+package com.czl.dollarpenguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarPenguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarPenguinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/czl/dollarpenguin/DollarPenguinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czl.dollarpenguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarPenguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
